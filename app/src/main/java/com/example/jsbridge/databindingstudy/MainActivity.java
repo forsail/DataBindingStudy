9333cd0e6cb69c6cc5601d3675f6b2ff55352e1b
lihong <lihong_sail@163.com>
2017-05-11 16:32:59 +0800

    opt

--- app/src/main/java/com/example/jsbridge/databindingstudy/MainActivity.java
+++ app/src/main/java/com/example/jsbridge/databindingstudy/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.jsbridge.databindingstudy;
 
 import android.databinding.DataBindingUtil;
-import android.databinding.ObservableField;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
